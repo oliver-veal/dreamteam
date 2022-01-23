fd531c1c4d61978500b1a3a444ea2eb701a8f9a7
Oliver Veal <vealys@gmail.com>
2022-01-23 11:24:49 +0000

    Logo colors change to black when circle moves out

--- src/lib/button/Button.svelte
+++ src/lib/button/Button.svelte
@@ -1,7 +1,4 @@
-<button
-  on:click
-  class="py-2 px-8 ring-2 ring-white rounded-full hover:ring-4 hover:ring-white transition-all"
->
+<button on:click class="py-2 px-8 ring-2 ring-white rounded-full">
   <span class="uppercase">
     <slot />
   </span>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,35 +1,42 @@
 <script lang="ts">
-  import { assets } from '$app/paths';
   import Button from '$lib/button/Button.svelte';
 
-  import Hero from './_hero.svelte';
+  import ImperialLogo from '$lib/logo/imperial-logo.svelte';
+  import Menu from '$lib/logo/menu.svelte';
 
   let scrollY: number;
+  let innerHeight: number;
+
+  $: console.log(scrollY / innerHeight);
 </script>
 
-<svelte:window bind:scrollY />
+<svelte:window bind:scrollY bind:innerHeight />
 
 <div
-  class="w-full h-full p-10 fixed inset-0 flex flex-col justify-between  z-50"
+  class="w-full h-full p-10 fixed inset-0 flex flex-col justify-between z-10"
 >
   <div class="flex items-center justify-between">
-    <img src="{assets}/dreamteam-logo.svg" alt="Dreamteam Logo" width="200" />
-    <img src="{assets}/menu.svg" alt="Menu" width="30" />
+    <h1 class="text-5xl">dreamteam</h1>
+    <Menu width="30" fill="#fff" />
   </div>
   <div class="flex items-center justify-between">
-    <img src="{assets}/imperial-logo.svg" alt="Dreamteam Logo" width="200" />
+    <ImperialLogo width="200" fill="#fff" />
+
     <Button>Contact</Button>
   </div>
 </div>
 
-<div class="absolute w-full h-full flex items-center justify-center">
-  <Hero />
+<div class="fixed w-full h-full flex items-center justify-center">
+  <h1 class="text-6xl uppercase font-light">
+    Design is more than beauty.<br />
+    Engineering is more than science.
+  </h1>
 </div>
 
 <div
-  class="h-screen w-screen fixed inset-0"
+  class="h-screen w-screen fixed inset-0 z-20"
   style="
-    background: linear-gradient(-45deg, #c850c0, #4158d0);
+    background: linear-gradient(-45deg, #ffd3e3, #ffb4f8, #cdaaff);
     clip-path: url(#myClip);
   "
 />
@@ -37,7 +44,30 @@
 <svg width="100vw" height="100vh" class="fixed inset-0">
   <defs>
     <clipPath id="myClip">
-      <circle cx="50%" cy="75%" r={scrollY} />
+      <circle cx="50%" cy="50%" r="{(scrollY * 100) / innerHeight}%" />
     </clipPath>
   </defs>
 </svg>
+
+<div
+  class="w-full h-full p-10 fixed inset-0 flex items-center justify-center z-30 overflow-hidden text-black"
+  style="clip-path: url(#myClip);"
+>
+  <div class="w-full h-full p-10 fixed inset-0 flex flex-col justify-between">
+    <div class="flex items-center justify-between">
+      <h1 class="text-5xl">dreamteam</h1>
+      <Menu width="30" fill="#000" />
+    </div>
+    <div class="flex items-center justify-between">
+      <ImperialLogo width="200" fill="#000" />
+      <button on:click class="py-2 px-8 ring-2 ring-black rounded-full">
+        <span class="uppercase"> Contact </span>
+      </button>
+    </div>
+  </div>
+
+  <h1 class="text-6xl uppercase font-light">
+    Design is more than beauty.<br />
+    Engineering is more than science.
+  </h1>
+</div>
