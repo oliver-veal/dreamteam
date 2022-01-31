6eb8c6807a0ed973fa37631ff2dc6ec209765c60
Oliver Veal <vealys@gmail.com>
2022-01-31 14:54:09 +0000

    How we do it section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,6 +18,10 @@
 <!-- <svelte:window bind:innerHeight on:load={() => window.scrollTo({ top: 0 })} /> -->
 <svelte:window bind:innerHeight />
 
+<head>
+  <title> Dreamteam </title>
+</head>
+
 <div
   class="w-full h-full p-10 fixed inset-0 flex flex-col justify-between z-50 pointer-events-none"
   in:scale={{ duration: 1500, start: 0.9, delay: 3000 }}
@@ -296,6 +300,93 @@
         rx="168.9"
         ry="64.37"
         transform="translate(200, 160) rotate(-63.76)"
+      />
+    </clipPath>
+  </defs>
+</svg>
+
+<Parallax sections={1} threshold={{ top: 0 }} style="background-color: black;">
+  <ParallaxLayer
+    rate={0.1}
+    style="display: flex; align-items: center; justify-content: center;"
+  >
+    <div class="flex w-full h-full justify-center">
+      <div class="flex flex-col text-center">
+        <h1 class="text-6xl uppercase pt-48">How we do it</h1>
+      </div>
+    </div>
+  </ParallaxLayer>
+  <ParallaxLayer
+    rate={0.2}
+    style="display: flex; flex-direction: column; align-items: center; justify-content: center;"
+  >
+    <div class="flex w-full h-full items-start justify-center pt-96">
+      <div
+        class="relative h-[86px] w-[971px]"
+        style="
+          background: linear-gradient(-20deg, rgba(255,188,85,1) 0%, rgba(223,0,56,1) 50%, rgba(158,65,208,1) 100%;
+          clip-path: url(#hwdi-clip);
+        "
+      />
+    </div>
+  </ParallaxLayer>
+
+  <ParallaxLayer
+    rate={0.3}
+    style="display: flex; flex-direction: column; align-items: center; justify-content: center;"
+  >
+    <div class="flex w-full h-full items-start justify-center pt-[450px]">
+      <div
+        class="relative h-[86px] w-[1200px] flex items-center justify-between"
+      >
+        <h2 class="font-bold text-3xl text-center w-1/4">Brief</h2>
+        <h2 class="font-bold text-3xl text-center w-1/4">Discover</h2>
+        <h2 class="font-bold text-3xl text-center w-1/4">Ideation</h2>
+        <h2 class="font-bold text-3xl text-center w-1/4">Delivery</h2>
+      </div>
+    </div>
+  </ParallaxLayer>
+  <ParallaxLayer
+    rate={0.4}
+    style="display: flex; flex-direction: column; align-items: center; justify-content: center;"
+  >
+    <div class="flex w-full h-full items-start justify-center pt-[550px]">
+      <div class="relative w-[1200px] flex justify-between text-center">
+        <p class="w-1/4 px-10">
+          You tell us what the challenge is, what you need and why. If you don't
+          know the details yet, we can help you define the brief so we know what
+          we're aiming to deliver.
+        </p>
+        <p class="w-1/4 px-10">
+          Working with you, we scope out the challenge. Through methods like
+          desk research, user research, interviews and market research, we
+          identify what the challenge context is, helping us to identify and
+          prioritise the specific challenges we need to solve and the key
+          directions we need to explore.
+        </p>
+        <p class="w-1/4 px-10">
+          We identify potential solutions, illustrating and prototyping the
+          possibilities in an iterative and collaborative process with you. We
+          test our ideas where necessary to ensure we're confident that we have
+          an effective solution.
+        </p>
+        <p class="w-1/4 px-10">
+          We deliver the outcomes as required by the brief. The exact formula
+          for what we deliver and how we deliver it depends on the initial
+          challenge, but we work closely with you to deliver the solution that
+          you need.
+        </p>
+      </div>
+    </div>
+  </ParallaxLayer>
+</Parallax>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 971 86" class="h-0 w-0">
+  <defs>
+    <clipPath id="hwdi-clip">
+      <path
+        class="cls-1"
+        d="M928,86a43,43,0,0,1-41.13-30.42A15,15,0,0,0,872.55,45H688.45a15,15,0,0,0-14.32,10.58,43,43,0,0,1-82.26,0A15,15,0,0,0,577.55,45H393.45a15,15,0,0,0-14.32,10.58,43,43,0,0,1-82.26,0A15,15,0,0,0,282.55,45H98.45A15,15,0,0,0,84.13,55.58,43,43,0,1,1,84.28,31,15.34,15.34,0,0,0,99,42H281.65a15.91,15.91,0,0,0,15.19-11.44,43,43,0,0,1,82.32,0A15.91,15.91,0,0,0,394.35,42h182.3a15.91,15.91,0,0,0,15.19-11.44,43,43,0,0,1,82.32,0A15.91,15.91,0,0,0,689.35,42H872a15.34,15.34,0,0,0,14.74-11A43,43,0,1,1,928,86Z"
       />
     </clipPath>
   </defs>
