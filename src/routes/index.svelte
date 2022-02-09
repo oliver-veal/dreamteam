116b2dec71d366e19006142084c82f3e0e842100
Oliver Veal <vealys@gmail.com>
2022-02-09 16:23:50 +0000

    Colors and menu rotate

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-  import Button from '$lib/button/Button.svelte';
-  import { scale } from 'svelte/transition';
+  // import Button from '$lib/button/Button.svelte';
 
-  import ImperialLogo from '$lib/logo/imperial-logo.svelte';
   import Menu from '$lib/logo/menu.svelte';
   import Hero from '$lib/sections/hero.svelte';
   import WhatWeveMade from '$lib/sections/what-weve-made.svelte';
@@ -12,20 +10,28 @@
   import WhatWeDo from '$lib/sections/what-we-do.svelte';
   import WhoWeAre from '$lib/sections/who-we-are.svelte';
   // import Manifesto from '$lib/sections/manifesto.svelte';
+
+  let menuOpen = false;
 </script>
 
 <head>
   <title> Dreamteam </title>
 </head>
 
-<div class="w-full h-0 fixed inset-0 flex flex-col justify-between z-10">
+<div class="w-full h-0 fixed inset-0 flex flex-col justify-between z-50">
   <div
-    class="flex items-center justify-between bg-black bg-opacity-75 backdrop-blur-lg p-2.5 md:p-5 lg:p-10"
+    class="flex items-center justify-between bg-black bg-opacity-75 backdrop-blur-lg p-2.5 md:p-5"
   >
-    <h1 class="text-3xl lg:text-5xl">
+    <h1 class="text-3xl lg:text-5xl select-none">
       dream<span class="font-light">team</span>
     </h1>
-    <Menu class="w-6 lg:w-8" fill="#fff" />
+    <button on:click={() => (menuOpen = !menuOpen)} class="p-2">
+      <Menu
+        class="w-6 lg:w-8 {menuOpen &&
+          'rotate-90'} transition-transform duration-300"
+        fill="#fff"
+      />
+    </button>
   </div>
 </div>
 
