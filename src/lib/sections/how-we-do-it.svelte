e59a4825f8607c9f4bbe495309e28dc6da72066c
Oliver Veal <vealys@gmail.com>
2022-02-09 10:40:42 +0000

    Safari (and mobile chrome) fixes

--- src/lib/sections/how-we-do-it.svelte
+++ src/lib/sections/how-we-do-it.svelte
@@ -7,73 +7,70 @@
   threshold={{ top: 0, bottom: 0 }}
   style="background-color: black;"
 >
-  <ParallaxLayer
-    rate={0.1}
-    style="display: flex; align-items: start; justify-content: start;"
-  >
-    <div class="flex w-full  h-[25%] xl:h-[40%] justify-center items-center">
-      <div class="flex flex-col text-center">
-        <h1 class="text-3xl xl:text-6xl uppercase">How we do it</h1>
+  <ParallaxLayer rate={0.1}>
+    <div class="flex w-full h-full justify-start items-start">
+      <div class="flex w-full  h-[25%] xl:h-[40%] justify-center items-center">
+        <div class="flex flex-col text-center">
+          <h1 class="text-3xl xl:text-6xl uppercase">How we do it</h1>
+        </div>
       </div>
     </div>
   </ParallaxLayer>
 
-  <ParallaxLayer
-    rate={0.2}
-    style="display: flex; flex-direction: column; align-items: start; justify-content: start;"
-  >
-    <div class="flex w-full h-[30%] sm:h-[40%] items-end justify-center px-5">
-      <img src="/hwdi/nodes.png" alt="How we do it" />
+  <ParallaxLayer rate={0.2}>
+    <div class="flex w-full h-full justify-start items-start">
+      <div class="flex w-full h-[30%] sm:h-[40%] items-end justify-center px-5">
+        <img src="/hwdi/nodes.png" alt="How we do it" />
+      </div>
     </div>
   </ParallaxLayer>
 
-  <ParallaxLayer
-    rate={0.3}
-    style="display: flex; align-items: end; justify-content: center;"
-  >
-    <div
-      class="w-[1200px] h-[60%] sm:h-[40%] flex flex-col sm:flex-row justify-start sm:justify-between items-start space-y-5 sm:space-y-0"
-    >
+  <ParallaxLayer rate={0.3}>
+    <div class="flex w-full h-full justify-center items-end">
       <div
-        class="w-full sm:w-1/4 px-5 sm:px-5 flex flex-col text-center space-y-2 sm:space-y-5"
+        class="w-[1200px] h-[60%] sm:h-[40%] flex flex-col sm:flex-row justify-start sm:justify-between items-start space-y-5 sm:space-y-0"
       >
-        <h2 class="font-bold text-xl xl:text-3xl">Brief</h2>
-        <p class="text-sm sm:text-base">
-          You tell us what the challenge is, what you need and why. If you don't
-          know the details yet, we can help you define the brief so we know what
-          we're aiming to deliver.
-        </p>
-      </div>
-      <div
-        class="w-full sm:w-1/4 px-5 sm:px-5 flex flex-col text-center space-y-2 sm:space-y-5"
-      >
-        <h2 class="font-bold text-xl xl:text-3xl">Discover</h2>
-        <p class="text-sm sm:text-base">
-          Working with you, we scope out the challenge. Through methods like
-          desk, user and market research, we identify context, helping us to
-          prioritise solutions and the key directions we need to explore.
-        </p>
-      </div>
-      <div
-        class="w-full sm:w-1/4 px-5 sm:px-5 flex flex-col text-center space-y-2 sm:space-y-5"
-      >
-        <h2 class="font-bold text-xl xl:text-3xl">Ideation</h2>
-        <p class="text-sm sm:text-base">
-          We identify potential solutions, illustrating and prototyping the
-          possibilities in an iterative and collaborative process. We test ideas
-          to ensure confidence in an effective solution.
-        </p>
-      </div>
-      <div
-        class="w-full sm:w-1/4 px-5 sm:px-5 flex flex-col text-center space-y-2 sm:space-y-5"
-      >
-        <h2 class="font-bold text-xl xl:text-3xl">Delivery</h2>
-        <p class="text-sm sm:text-base">
-          We deliver the outcomes as required by the brief. The exact formula
-          for what we deliver and how we deliver it depends on the initial
-          challenge, but we work closely with you to deliver the solution that
-          you need.
-        </p>
+        <div
+          class="w-full sm:w-1/4 px-5 sm:px-5 flex flex-col text-center space-y-2 sm:space-y-5"
+        >
+          <h2 class="font-bold text-xl xl:text-3xl">Brief</h2>
+          <p class="text-sm sm:text-base">
+            You tell us what the challenge is, what you need and why. If you
+            don't know the details yet, we can help you define the brief so we
+            know what we're aiming to deliver.
+          </p>
+        </div>
+        <div
+          class="w-full sm:w-1/4 px-5 sm:px-5 flex flex-col text-center space-y-2 sm:space-y-5"
+        >
+          <h2 class="font-bold text-xl xl:text-3xl">Discover</h2>
+          <p class="text-sm sm:text-base">
+            Working with you, we scope out the challenge. Through methods like
+            desk, user and market research, we identify context, helping us to
+            prioritise solutions and the key directions we need to explore.
+          </p>
+        </div>
+        <div
+          class="w-full sm:w-1/4 px-5 sm:px-5 flex flex-col text-center space-y-2 sm:space-y-5"
+        >
+          <h2 class="font-bold text-xl xl:text-3xl">Ideation</h2>
+          <p class="text-sm sm:text-base">
+            We identify potential solutions, illustrating and prototyping the
+            possibilities in an iterative and collaborative process. We test
+            ideas to ensure confidence in an effective solution.
+          </p>
+        </div>
+        <div
+          class="w-full sm:w-1/4 px-5 sm:px-5 flex flex-col text-center space-y-2 sm:space-y-5"
+        >
+          <h2 class="font-bold text-xl xl:text-3xl">Delivery</h2>
+          <p class="text-sm sm:text-base">
+            We deliver the outcomes as required by the brief. The exact formula
+            for what we deliver and how we deliver it depends on the initial
+            challenge, but we work closely with you to deliver the solution that
+            you need.
+          </p>
+        </div>
       </div>
     </div>
   </ParallaxLayer>
