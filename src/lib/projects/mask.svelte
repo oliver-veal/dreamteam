e44d9abd27782c8290ac4a5ea7ed5f0ba212781e
Oliver Veal <vealys@gmail.com>
2022-02-10 12:30:58 +0000

    Projecs

--- src/lib/projects/mask.svelte
+++ src/lib/projects/mask.svelte
@@ -1,51 +1,47 @@
 <div
-  class="relative min-w-full flex flex-col px-10 bg-black items-center justify-start mt-4 xl:mt-0"
+  class="flex flex-col xl:flex-row space-y-10 xl:space-y-0 xl:space-x-10 items-center justify-center p-10"
 >
+  <div class="relative xl:mt-0">
+    <img
+      src="/projects/mask/facemask.svg"
+      alt="Face Mask Project Logo"
+      class="absolute inset-0 w-48 xl:w-72 xl:-translate-x-1/3 -translate-x-[5%] -translate-y-1/4"
+    />
+    <img
+      src="/projects/mask/facemask.png"
+      alt="Face Mask Project"
+      class="w-full max-h-[411px] object-cover"
+    />
+  </div>
   <div
-    class="flex flex-col xl:flex-row space-y-10 xl:space-y-0 xl:space-x-10 items-center justify-center"
+    class="flex flex-col space-y-5 max-w-[600px] xl:px-0 xl:max-w-[400px] h-full justify-center"
   >
-    <div class="relative  xl:mt-0">
-      <img
-        src="/projects/mask/facemask.svg"
-        alt="Face Mask Project Logo"
-        class="absolute inset-0 w-48 xl:w-72 xl:-translate-x-1/3 -translate-x-[5%] -translate-y-1/4"
-      />
-      <img
-        src="/projects/mask/facemask.png"
-        alt="Face Mask Project"
-        class="w-full max-h-[411px] object-cover"
-      />
+    <div class="flex flex-col space-y-2.5">
+      <h2 class="text-4xl">Design</h2>
+      <q class="text-xs">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy
+      </q>
     </div>
-    <div
-      class="flex flex-col space-y-5 max-w-[600px] xl:px-0 xl:max-w-[400px] h-full justify-center"
-    >
-      <div class="flex flex-col space-y-2.5">
-        <h2 class="text-4xl">Design</h2>
-        <q class="text-xs">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy
-        </q>
-      </div>
 
-      <div class="flex flex-col space-y-5">
-        <h3 class="text-lg">Brief</h3>
-        <p class="text-justify text-sm">
-          To find new ways to make students feel safe and at home when they
-          arrive at Imperial admist the COVID-19 pandemic.
-        </p>
-        <h3 class="text-lg">Impact</h3>
-        <p class="text-justify text-sm">
-          Given that masks are a necessity in public spaces we created a face
-          covering that not only provides exceptional protection to students but
-          also makes them feel part of the wider Imperial community. We designed
-          4 unique designs that represent the values and experiences of Imperial
-          students, and our HeiQ Viroblock equipped masks provided a seriously
-          high level of protection. In support, we designed a marketing
-          campaign, created posters, and directed and filmed a promotional video
-          for students. Currently, we have sold over 1000 masks with a reorder
-          approved.
-        </p>
-      </div>
+    <div class="flex flex-col space-y-5">
+      <h3 class="text-lg">Brief</h3>
+      <p class="text-justify text-sm">
+        To find new ways to make students feel safe and at home when they arrive
+        at Imperial admist the COVID-19 pandemic.
+      </p>
+      <h3 class="text-lg">Impact</h3>
+      <p class="text-justify text-sm">
+        Given that masks are a necessity in public spaces we created a face
+        covering that not only provides exceptional protection to students but
+        also makes them feel part of the wider Imperial community. We designed 4
+        unique designs that represent the values and experiences of Imperial
+        students, and our HeiQ Viroblock equipped masks provided a seriously
+        high level of protection. In support, we designed a marketing campaign,
+        created posters, and directed and filmed a promotional video for
+        students. Currently, we have sold over 1000 masks with a reorder
+        approved.
+      </p>
     </div>
   </div>
 </div>
--- src/lib/sections/what-weve-made.svelte
+++ src/lib/sections/what-weve-made.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
+  import { slide, fade } from 'svelte/transition';
   import Mask from '$lib/projects/mask.svelte';
-  import { slide } from 'svelte/transition';
-  import X from '$lib/icon/heroicons/X.js';
   import Icon from '$lib/icon/Icon.svelte';
+  import X from '$lib/icon/heroicons/X.js';
 
   let maskOpen = false;
 </script>
 
 <section
-  class="relative w-full flex flex-col space-y-24 xl:space-y-48 left-0"
+  class="w-full flex flex-col p-10 space-y-24 xl:space-y-48 items-center"
   id="what-weve-made"
 >
   <div class="w-full text-center">
@@ -17,63 +17,61 @@
   </div>
 
   <div
-    class="{maskOpen &&
-      '-translate-x-[100vw]'} transition-transform duration-300 w-[200vw] flex justify-center"
+    class="flex flex-col sm:flex-row space-y-12 sm:space-y-0 w-full items-center justify-between space-x-5 max-w-[800px]"
   >
-    <div transition:slide class="flex w-[100vw] justify-center items-start">
-      <div
-        class="flex flex-col sm:flex-row space-y-12 sm:space-y-0 w-full max-w-[800px] space-x-5 px-5 justify-between"
-      >
-        <button
-          class="relative group cursor-pointer"
-          on:click={() => (maskOpen = true)}
-        >
-          <img
-            src="/what-weve-made/mask.png"
-            alt="Face Mask Project"
-            class="max-h-[325px] object-cover group-hover:-translate-y-8 transition-transform duration-300"
-          />
-          <div class="absolute bottom-0 left-0 flex flex-col space-y-2.5 z-10">
-            <img src="/wwd/mask.svg" class="w-10 h-10" alt="Mask" />
-            <p class="font-bold text-lg">Imperial Face Mask</p>
-          </div>
-        </button>
-
-        <div class="relative group cursor-pointer flex items-center">
-          <img
-            src="/what-weve-made/bic.png"
-            alt="Blast Injury Conference Project"
-            class="max-h-[325px] object-cover group-hover:-translate-y-8 transition-transform duration-300"
-          />
-          <div class="absolute bottom-0 left-0 flex flex-col space-y-2.5 z-10">
-            <img src="/wwd/bic.svg" class="w-10 h-10" alt="BIC" />
-            <p class="font-bold text-lg">Blast Injury Conference</p>
-          </div>
-        </div>
-
-        <div class="relative group cursor-pointer">
-          <img
-            src="/what-weve-made/bmx.png"
-            alt="BMX Project"
-            class="max-h-[325px] object-cover group-hover:-translate-y-8 transition-transform duration-300"
-          />
-          <div class="absolute bottom-0 left-0 flex flex-col space-y-2.5 z-10">
-            <img src="/wwd/bmx.svg" class="w-10 h-10" alt="BMX" />
-            <p class="font-bold text-lg">BMX Project</p>
-          </div>
-        </div>
+    <button
+      class="relative group cursor-pointer"
+      on:click={() => (maskOpen = true)}
+    >
+      <img
+        src="/what-weve-made/mask.png"
+        alt="Face Mask Project"
+        class="w-full max-h-[325px] object-cover group-hover:-translate-y-8 transition-transform duration-300"
+      />
+      <div class="absolute bottom-0 left-0 flex flex-col space-y-2.5 z-10">
+        <img src="/wwd/mask.svg" class="w-10 h-10" alt="Mask" />
+        <p class="font-bold text-lg">Imperial Face Mask</p>
+      </div>
+    </button>
+    <div class="relative group cursor-pointer">
+      <img
+        src="/what-weve-made/bic.png"
+        alt="Blast Injury Conference Project"
+        class="w-full max-h-[325px] object-cover group-hover:-translate-y-8 transition-transform duration-300"
+      />
+      <div class="absolute bottom-0 left-0 flex flex-col space-y-2.5 z-10">
+        <img src="/wwd/bic.svg" class="w-10 h-10" alt="BIC" />
+        <p class="font-bold text-lg">Blast Injury Conference</p>
       </div>
     </div>
-
-    <div class="flex flex-col w-[100vw]">
-      <div class="w-full static flex items-center justify-end z-40">
-        <button on:click={() => (maskOpen = false)} class="p-2.5 md:p-5">
-          <Icon src={X} class="w-12 xl:w-14" />
-        </button>
+    <div class="relative group cursor-pointer">
+      <img
+        src="/what-weve-made/bmx.png"
+        alt="BMX Project"
+        class="w-full max-h-[325px] object-cover group-hover:-translate-y-8 transition-transform duration-300"
+      />
+      <div class="absolute bottom-0 left-0 flex flex-col space-y-2.5 z-10">
+        <img src="/wwd/bmx.svg" class="w-10 h-10" alt="BMX" />
+        <p class="font-bold text-lg">BMX Project</p>
       </div>
-      {#if maskOpen}
-        <Mask />
-      {/if}
     </div>
   </div>
 </section>
+
+{#if maskOpen}
+  <div class="absolute inset-0">
+    <div
+      transition:slide
+      class="w-screen h-screen overflow-y-auto fixed inset-0 bg-black flex flex-col justify-center z-20 py-24"
+    >
+      <div class="w-full h-full flex flex-col justify-center items-center">
+        <button on:click={() => (maskOpen = false)} class="p-2.5 md:p-5">
+          <Icon src={X} class="w-12 xl:w-14" />
+        </button>
+        <div transition:fade>
+          <Mask />
+        </div>
+      </div>
+    </div>
+  </div>
+{/if}
